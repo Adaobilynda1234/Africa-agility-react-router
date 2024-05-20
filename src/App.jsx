17b9a71c05c8301507f192cae86f0cbb6e801b29
Adaobilynda1234 <adaobilynda1234@gmail.com>
2024-05-20 16:34:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Service from "./pages/Service";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
